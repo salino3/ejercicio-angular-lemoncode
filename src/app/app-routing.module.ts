efd013f5db57bd3697dca2c40e2751736e63c353
salino3 <flaviobcn012@gmail.com>
2023-04-17 10:23:42 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './scenes/home/home.component';
+import { LoginPageComponent } from './scenes/login-page/login-page.component';
+import { AboutComponent } from './scenes/about/about.component';
+import { DashboardComponent } from './scenes/dashboard/dashboard.component';
+import { GalleryComponent } from './scenes/gallery/gallery.component';
+import { ProfileComponent } from './scenes/profile/profile.component';
+import { CRUDComponent } from './scenes/crud/crud.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'crud', component: CRUDComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { PublicHeaderComponent } from './layouts/public-header/public-header.component';
+import { PrivateHeaderComponent } from './layouts/private-header/private-header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { PublicMenuComponent } from './layouts/public-menu/public-menu.component';
+import { PrivateMenuComponent } from './layouts/private-menu/private-menu.component';
+import { HomeComponent } from './scenes/home/home.component';
+import { LoginPageComponent } from './scenes/login-page/login-page.component';
+import { AboutComponent } from './scenes/about/about.component';
+import { GalleryComponent } from './scenes/gallery/gallery.component';
+import { DashboardComponent } from './scenes/dashboard/dashboard.component';
+import { ProfileComponent } from './scenes/profile/profile.component';
+import { CRUDComponent } from './scenes/crud/crud.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PublicHeaderComponent,
+    PrivateHeaderComponent,
+    FooterComponent,
+    PublicMenuComponent,
+    PrivateMenuComponent,
+    LoginPageComponent,
+    AboutComponent,
+    GalleryComponent,
+    DashboardComponent,
+    ProfileComponent,
+    CRUDComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
