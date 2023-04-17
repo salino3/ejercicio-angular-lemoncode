6e8477225ae13fb49c4815f3415ba6cc35f09773
salino3 <flaviobcn012@gmail.com>
2023-04-17 18:45:29 +0200

    changes in login-page

--- src/app/scenes/login-page/login-page.component.ts
+++ src/app/scenes/login-page/login-page.component.ts
@@ -2,12 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {
   FormGroup,
   FormBuilder,
-  AbstractControl,
   Validators,
 } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
-
 
 
 @Component({
